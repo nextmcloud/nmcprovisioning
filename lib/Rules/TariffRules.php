bad426a5b33aadf2bcc27544aa3ac8f942542f07
Bernd.Rederlechner@t-systems.com <bernd.rederlechner@t-systems.com>
2023-08-13 09:41:17 +0200

    Cleaned code and unittests, all green

--- lib/Rules/TariffRules.php
+++ lib/Rules/TariffRules.php
@@ -23,28 +23,28 @@ class TariffRules {
 	/**
 	 * Quota computation helper function to check
 	 * claims contain none of the configured flags
-     * 
-     * For random combinations of tariffs where even one of
-     * the flaged tariffs can be 0, the case must be explixitly checked
-     * and cannot be used as default.
+	 *
+	 * For random combinations of tariffs where even one of
+	 * the flaged tariffs can be 0, the case must be explixitly checked
+	 * and cannot be used as default.
 	 */
 	private function quotaNoFlags(object $claims, $tariffs) {
-        $anyFlagSet = array_reduce($tariffs, function ($carry, $tariff) use ($claims) {
-            if (array_key_exists('flag', $tariff) && ($carry == false)) {
-                return $this->isQuotaFlagSet($claims, $tariff);
-            } else {
-                return $carry;
-            }
-        }, false);
+		$anyFlagSet = array_reduce($tariffs, function ($carry, $tariff) use ($claims) {
+			if (array_key_exists('flag', $tariff) && ($carry == false)) {
+				return $this->isQuotaFlagSet($claims, $tariff);
+			} else {
+				return $carry;
+			}
+		}, false);
 
-        if (!$anyFlagSet) {
-            // make sure that a quota >0 is only delivered
-            // for the NOFLAGS case if actually no flags are set
-            // Otherwise, all quotas < the NOFLAGS quota will not apply
-            return $tariffs['NOFLAGS']['space_limit'];
-        } else {
-            return "0 B";
-        }
+		if (!$anyFlagSet) {
+			// make sure that a quota >0 is only delivered
+			// for the NOFLAGS case if actually no flags are set
+			// Otherwise, all quotas < the NOFLAGS quota will not apply
+			return $tariffs['NOFLAGS']['space_limit'];
+		} else {
+			return "0 B";
+		}
 	}
 
 
@@ -88,13 +88,13 @@ class TariffRules {
 			if (array_key_exists('flag', $tariff)) {
 				return $this->isQuotaFlagSet($claims, $tariff) ? $tariff['space_limit'] : "0 B";
 			} else {
-                // skip the "no flags" case
-                // it is the deafult value of reduce
+				// skip the "no flags" case
+				// it is the deafult value of reduce
 				return "0 B";
 			}
 		}, $tariffs);
 
-        $noflagsQuota = $this->quotaNoFlags($claims, $tariffs);
+		$noflagsQuota = $this->quotaNoFlags($claims, $tariffs);
 		$maxQuota = array_reduce($quotaCandidates, array($this, 'maxQuota'), $noflagsQuota);
 
 		// Return the max quota limit as human readable unit
